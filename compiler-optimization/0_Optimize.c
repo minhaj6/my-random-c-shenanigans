3e88a01cd988c96fcf031a9d33ad951c6d5562c7
minhaj <minhaj.sixbyte@gmail.com>
2023-10-14 13:38:08 +0600

    optimization gcc

---
+++ compiler-optimization/0_Optimize.c
@@ -0,0 +1,12 @@
+/*
+* Compiler Optimizations - 
+* - Loop unrolling, splitting, fusion
+* - Function inlining
+* - Precompute values
+* - Reordering operatinos
+
+* gcc -O3 0_Optimize.c. O can have three levels O {0,1,2,3}
+
+* -Ofast tries to optimize even more with non-standard stuff
+* -O2 is a good choice. Linux kernel uses -O2
+*/
